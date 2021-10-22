243774cd6bcfc5f63a6fb8b955dd453ddaec438c
devspitz <spitzdev@gmail.com>
2021-10-21 22:57:03 -0400

    hmwk 72

--- hmwk 72/homework 72/recipes.js
+++ hmwk 72/homework 72/recipes.js
@@ -1,37 +1,51 @@
 (function () {
 
-    function recipes() {
+    function getRecipeList() {
         fetch('recipesAvailable.json')
             .then(r => {
                 if (!r.ok) {
-                    throw new Error(`${r.status} ${r.statusText}`);
+                    throw new Error(`${r.status} ${r.statusText}`)
                 }
                 return r.json();
             })
-            .then(theRecipes => {
+            .then(function (theRecipes) {
                 theRecipes.forEach((recipe) => {
-                    $('#recipeList').append('<label> ${recipe} </label>');
+                    const label = $(`<label>${recipe}</label>`);
+                    $('#recipeList').append(`<input type="radio" name="recipeChoice" id="the${recipe}" value="${recipe}">`);
+                    $('#recipeList').append(label);
+                });
 
+                $('input[name="recipeChoice"]').change(function () {
+                    $('.textDisplayer').empty();
+                    const theRecipe = $('input[name="recipeChoice"]:checked').val();
+
+                    fetch(`${theRecipe}.json`)
+                        .then(r => {
+                            if (!r.ok) {
+                                throw new Error(`${r.status} ${r.statusText}`);
+                            }
+                            return r.json();
+                        })
+                        .then(recipeFile => {
+                            console.log(recipeFile);
+                            $('.textDisplayer').append(`<div>${recipeFile.name}</div>`);
+                            $('.textDisplayer').append(`<div>${recipeFile.ingredients}</div>`)
+                            $('.textDisplayer').append(`<div><img src="${recipeFile.img}"></div>`);
+
+                        })
+                        .catch((err) => {
+                            console.log('oops');
+                        });
                 });
             })
-            .catch((err) => {
-                console.log('oops');
+            .catch((error) => {
+                console.log(error, 'oops');
             });
-    } ();
 
-    $('#optionOneButton').click(() => {
-        fetch('brownies.json')
-            .then(r => {
-                if (!r.ok) {
-                    throw new Error(`${r.status} ${r.statusText}`);
-                }
-                return r.json();
-            })
-            .then(brownie => {
-                $('#theDisplayer'), append(brownie);
-            })
-            .catch((err) => {
-                console.log('oops');
-            });
-    })
+
+
+    };
+    getRecipeList();
+
+
 }());
--- hmwk 72/weather.js
+++ hmwk 72/weather.js
@@ -1,8 +1,40 @@
 (function () {
-    const zip = $('#zip');
-    zip.change(() => {
-        fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${zip.val()}&appid=b71053d6a23e594f7a4d49f3b8c74e52`)
-            .then(r => r.json())
-            .then(weather => console.log(weather))
+    'use strict';
+    const appID = 'b71053d6a23e594f7a4d49f3b8c74e52';
+
+    const zipInput = $('#zip');
+    const location = $('#location');
+    const details = $('#details');
+    const icon = $('#icon');
+
+    const noWeather = $('.noWeather')
+    const yesWeather = $('.hasWeather')
+    const error = $('.error')
+
+    yesWeather.hide();
+    $('form').submit(e => e.preventDefault());
+
+    zipInput.change(() => {
+        fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${zipInput.val()}&appid=${appID}`)
+            .then(r => {
+                if (!r.ok) {
+                    throw new Error(`${r.status} ${r.statusText}`);
+                }
+                return r.json();
+            })
+            .then(weather => {
+                noWeather.hide();
+                yesWeather.show();
+
+                console.log(weather);
+                location.text(weather.name);
+                icon.attr('src', `http://openweathermap.org/img/wn/${weather.weather[0].icon}.png`);
+                details.text(`${weather.main.temp} and ${weather.weather[0].description}`);
+            })
+            .catch(e => {
+                noWeather.show();
+                yesWeather.hide();
+                error.text(e.message)
+            });
     });
 }());
