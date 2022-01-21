fd2c213ab339d1bbf15cb7d1e57b54ef26cae8eb
devspitz <spitzdev@gmail.com>
2022-01-20 22:10:47 -0500

    89 done!

--- 89/weather/src/App.js
+++ 89/weather/src/App.js
@@ -1,47 +1,46 @@
 import './App.css';
 import React, { Component } from 'react';
 import WeatherDetails from './WeatherDetails.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
+  state = {
+    zips: ['44118', '08701'],
+    DataisLoaded: false
+  };
 
-    this.state = {
-      items: [],
-      DataisLoaded: false
-    };
-  }
-
-  componentDidMount(theZip) {
-    theZip = '44118';
-      fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${theZip}&appid=cb7c71219cf09eb0bb414b932669be97&units=imperial`)
-        .then((res) => res.json())
-        .then((weatherData) => {
-            this.setState({     
+  componentDidMount() {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${this.state.zips[0]}&appid=cb7c71219cf09eb0bb414b932669be97&units=imperial`)
+      .then((res) => res.json())
+      .then((weatherData) => {
+        this.setState({
           items: weatherData,
           DataisLoaded: true
-            });
-  })
-}
+        });
+      })
+  }
   render() {
     const { DataisLoaded, items } = this.state;
-   console.log(items);
+    console.log(items);
     if (!DataisLoaded) return <div> <h1> Pleses wait some time.... </h1> </div>;
-   
 
     return (
-      
       <div className="App">
         <div className="container text-center">
           <h1>Choose a US Zip to see the weather</h1>
+<div>
           <ul>
-            <li><button className="btn btn-primary">08701</button></li>
-            <li><button className="btn btn-primary">44118</button></li>
-            <li><button className="btn btn-primary">44122</button></li>
+            <li><button className="btn btn-dark">08701</button></li>
+            <li><button className="btn btn-dark">44118</button></li>
+            <li><button className="btn btn-dark">44122</button></li>
           </ul>
+            </div>
+            <div className="container">
+          <input className='input-group input-group-sm mb-3 col-md-auto' placeholder='or enter a zip here' />
+          </div>
         </div>
-         <WeatherDetails weather={this.state.items[0]} />
-    
+        {this.state.items ? <WeatherDetails weather={this.state.items}/> : <h1> Please wait some time.... </h1>}
+
       </div>
     );
   }
--- 89/weather/src/WeatherDetails.js
+++ 89/weather/src/WeatherDetails.js
@@ -2,18 +2,12 @@ import React from "react";
 
 export default function WeatherDetails(props) {
 
-    const { name, main, weather } = props.weather;
-console.log(name);
+    const { name, weather, main } = props.weather;
+    console.log(props);
     return (
-        <ol>  
+        <ol className='weatherDisplay'>
             <li><h2>{name}</h2></li>
-
-            {main.map(details => (
-                <li><h2>{details.temp} <span>&#8457;</span></h2></li>
-            ))}
-        
-
-            <li> <p>The weather in {name} is currently {weather.map(weather => (
-                    <li><h2>{weather.description}</h2></li>))}.</p></li>
+            <li><img src={`https://openweathermap.org/img/w/${weather[0].icon}.png` } alt="theWeatherPicture"/></li>
+            <li> <p>The weather in {name} is currently {main.temp}.</p></li>
         </ol>)
 }
