840f3a9489cd2bc4ac5239544bde344ef3c50b6f
devspitz <spitzdev@gmail.com>
2022-03-29 11:49:29 -0400

    92 continuation

--- 92/recipes/src/RecipeDetails.js
+++ 92/recipes/src/RecipeDetails.js
@@ -2,24 +2,22 @@ import React, { useEffect, useState } from "react";
 import ListComponent from './ListComponent';
 
 export default function RecipeDetails(props) {
-    const [showPicture, setShowPicture] = useState({ showPicture: true });
+    const [showPicture, setShowPicture] = useState(true);
 
     useEffect(() => {
-        showPicture.setShowPicture(!showPicture)
-    }, [showPicture]);
+        setShowPicture(!showPicture)
+    }, [/*showPicture*/]);
 
-
-    const { name, ingredients, directions, picture } = this.props.recipe;
+    const { name, ingredients, directions, picture } = props;
 
     return (
         <>
             <h2>{name}</h2>
-            {this.state.showPicture && <img style={{ width: '200px', height: '200px' }} className="img-thumbnail" src={picture} alt={name} />}
+            {/*this.state.*/showPicture && <img style={{ width: '200px', height: '200px' }} className="img-thumbnail" src={picture} alt={name} />}
             <br />
-            <button onClick={this.togglePicture} className="btn btn-secondary">{this.state.showPicture ? 'hide' : 'show'}</button>
+            <button /*onClick={this.togglePicture}*/ className="btn btn-secondary">{/*this.state.*/showPicture ? 'hide' : 'show'}</button>
             <ListComponent title="Ingredients" items={ingredients} />
             <ListComponent title="directions" items={directions} />
         </>
     );
 }
-
